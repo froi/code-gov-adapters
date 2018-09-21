5c1e69e49327e99ad332a5b4dc0d5a31410af4c2
Froilan Irizarry <froilan@froilanirizarry.me>
2018-09-21 00:03:30 -0400

    Elasticsearch adapter tests initial commit

---
+++ tests/elasticsearch/elasticsearch.test.js
@@ -0,0 +1,52 @@
+const expect = require('chai').expect();
+const { ElasticsearchAdapter } = require('../../libs/elasticsearch')
+
+describe('Elasticsearch Adapter', () => {
+  describe('createIndex', () => {
+    describe('without settings', () => {
+      it('should return index information', ()=> {
+
+      });
+    });
+    describe('with settings', () => {
+      it('should return index information', ()=> {
+
+      });
+    });
+  });
+  describe('deleteIndex', () => {
+    it('should ', ()=> {
+
+    });
+  });
+  describe('indexExists', () => {
+    it('should ', ()=> {
+
+    });
+  });
+  describe('indexDocument', () => {
+    it('should ', ()=> {
+
+    });
+  });
+  describe('updateDocument', () => {
+    it('should ', ()=> {
+
+    });
+  });
+  describe('deleteDocument', () => {
+    it('should ', ()=> {
+
+    });
+  });
+  describe('getIndexForAlias', () => {
+    it('should ', ()=> {
+
+    });
+  });
+  describe('initIndexMapping', () => {
+    it('should ', ()=> {
+
+    });
+  });
+});
