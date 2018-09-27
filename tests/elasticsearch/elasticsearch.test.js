068c195bf6cc093dee2328444f464f23a6c3da00
Froilan Irizarry <froilan@froilanirizarry.me>
2018-09-27 11:58:46 -0400

    Added expect statements to have all test fail. These need to be implemented

--- tests/elasticsearch/elasticsearch.test.js
+++ tests/elasticsearch/elasticsearch.test.js
@@ -1,52 +1,65 @@
-const expect = require('chai').expect()
-const { ElasticsearchAdapter } = require('../../libs/elasticsearch')
+const expect = require('chai').expect();
+const { ElasticsearchAdapter } = require('../../libs/elasticsearch');
 
 describe('Elasticsearch Adapter', () => {
+  let adapter;
+  beforeEach(() => {
+    adapter = new ElasticsearchAdapter();
+  });
   describe('createIndex', () => {
     describe('without settings', () => {
       it('should return index information', () => {
-
-      })
-    })
+        // Needs to be implemented
+        expect(true).to.equal(false);
+      });
+    });
     describe('with settings', () => {
       it('should return index information', () => {
-
-      })
-    })
-  })
+        // Needs to be implemented
+        expect(true).to.equal(false);
+      });
+    });
+  });
   describe('deleteIndex', () => {
     it('should ', () => {
-
-    })
-  })
+      // Needs to be implemented
+      expect(true).to.equal(false);
+    });
+  });
   describe('indexExists', () => {
     it('should ', () => {
-
-    })
-  })
+      // Needs to be implemented
+      expect(true).to.equal(false);
+    });
+  });
   describe('indexDocument', () => {
     it('should ', () => {
-
-    })
-  })
+      // Needs to be implemented
+      expect(true).to.equal(false);
+    });
+  });
   describe('updateDocument', () => {
     it('should ', () => {
-
-    })
-  })
+      // Needs to be implemented
+      expect(true).to.equal(false);
+    });
+  });
   describe('deleteDocument', () => {
     it('should ', () => {
-
-    })
-  })
+      // Needs to be implemented
+      expect(true).to.equal(false);
+    });
+  });
   describe('getIndexForAlias', () => {
     it('should ', () => {
-
-    })
-  })
+      // Needs to be implemented
+      expect(true).to.equal(false);
+    });
+  });
   describe('initIndexMapping', () => {
     it('should ', () => {
-
-    })
-  })
-})
+      // Needs to be implemented
+      expect(true).to.equal(false);
+    });
+  });
+});
